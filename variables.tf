ef38d6a4aa503e3f1b70abd5b0a6e06d3d4d4f78
Gompei <shoueigonpei6028@gmail.com>
2021-11-03 10:35:06 +0900

    サーバーレスに修正

--- variables.tf
+++ variables.tf
@@ -14,9 +14,3 @@ variable "my_domain" {
   description = "My Domain."
   type        = string
 }
-
-variable "vpc_cidr" {
-  description = "The IP address range of the VPC in CIDR notation."
-  default     = "10.0.0.0/16"
-  type        = string
-}
