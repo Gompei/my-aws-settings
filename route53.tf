99eb48f2cee3002a1e892b9827a725ef6a825ab3
Gompei <shoueigonpei6028@gmail.com>
2021-11-07 10:38:13 +0900

    txtレコード追加

--- route53.tf
+++ route53.tf
@@ -9,3 +9,11 @@ resource "aws_route53_record" "gcp" {
   ttl     = 3600
   records = var.gcp_dns_ns_records
 }
+
+resource "aws_route53_record" "gcp_txt" {
+  zone_id = aws_route53_zone.gom60.id
+  name    = var.my_domain
+  type    = "TXT"
+  ttl     = 3600
+  records = var.gcp_dns_txt_records
+}
