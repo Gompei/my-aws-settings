537da12307c49f44eca9d5856d8c1879eb994ac1
Gompei <shoueigonpei6028@gmail.com>
2022-05-04 15:38:06 +0900

    VPNサーバー変数追加

--- route53.tf
+++ route53.tf
@@ -1,3 +1,11 @@
 resource "aws_route53_zone" "gom60" {
   name = var.my_domain
 }
+
+resource "aws_route53_record" "cloudfront_alias" {
+  zone_id = aws_route53_zone.gom60.zone_id
+  name    = var.vpn_server_record
+  type    = "A"
+  ttl     = "300"
+  records = [var.vpn_server_ip]
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,11 @@ variable "my_domain" {
   description = "My Domain."
   type        = string
 }
+
+variable "vpn_server_record" {
+  type = string
+}
+
+variable "vpn_server_ip" {
+  type = string
+}
